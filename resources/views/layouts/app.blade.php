c2d011a5feb4b6725db225ff59d2c6eabf4fc192
adriansctehc <adriansanchezcarchano@gmail.com>
2017-02-08 17:21:27 +0100

    prueba para comprobar que sandra ejecuta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,19 +66,18 @@
                     <ul class="nav navbar-nav">
                         &nbsp;
                     </ul>
-
+                   
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @if (Auth::guest())
                             <li><a href="{{ url('/login') }}">Login</a></li>
                             <li><a href="{{ url('/register') }}">Register</a></li>
-                        @else
+                        @else                           
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
-
                                 <ul class="dropdown-menu" role="menu">
                                     <li>
                                         <a href="{{ url('/logout') }}"
@@ -91,7 +90,7 @@
                                             {{ csrf_field() }}
                                         </form>
                                     </li>
-                                </ul>
+                                </ul>                                                       
                             </li>
                         @endif
                     </ul>
