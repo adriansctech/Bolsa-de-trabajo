b5546c684d90c36674165315b1d8d432870de8e5
sergiosellesb <sergioselles95@gmail.com>
2017-02-08 18:23:23 +0100

    vista alumno princ

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,25 @@
-@extends('layouts.app')
+@extends('layouts.general')
 
 @section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
+                <!-- cabecera de la pagina -->
+                <div class="panel-heading" >
+                    <img src="img/user.jpg" id="fotouser">
+                    <div id="datosuser">
+                            <label id="labelNombre">Nombre y Apellidos</label><br>
+                            <label id="labelNombre"></label><br>  
+                        <label id="labelCorreo">{{ Auth::User()->email }}</label>
+                    </div>
+                    <img src="img/editar.png" id="enlaceeditar">
+                </div>
+                <!-- contenido de la pagina -->
                 <div class="panel-body">
-                    You are logged in!
+                    <div id="ofertas">
+                        <h2 id="tituloofertas">Ofertas disponibles</h2>
+                    </div>  
                 </div>
             </div>
         </div>
