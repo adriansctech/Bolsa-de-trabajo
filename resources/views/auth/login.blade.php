bff654f9eb83711fa1e50a382d8f75c9ccdc7414
vagrant <vagrant@homestead>
2017-02-13 16:37:56 +0000

    Correccion login stilos

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container login">
+<div class="container ">
     <div class="row ">
         <div class="col-xs-6 col-xs-offset-3 col-md-8 col-md-offset-2 ">
             <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+                <div class="panel-heading login">Login</div>
+                <div class="panel-body login">
+                    <form class="form-horizontal " role="form" method="POST" action="{{ url('/login') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
