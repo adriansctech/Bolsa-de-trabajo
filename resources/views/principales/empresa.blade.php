8585913705b1c242c5ac1292a514408d3b7e4f69
vagrant <vagrant@homestead>
2017-02-13 17:50:52 +0000

    modificaciones en cssPrincipal

--- resources/views/principales/empresa.blade.php
+++ resources/views/principales/empresa.blade.php
@@ -3,23 +3,23 @@
 @section('content')
 <div class="container contenido">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
+        <div class="col-xs-12 col-md-10 col-md-offset-1">
+            <div class="panel panel-default ">
                 <!-- cabecera de la pagina -->
-                <div class="panel-heading" >
-                    <div class="infouser">
+                <div class="panel-heading " >
+                    <div class="infouser ">
                         <img src="img/user.jpg" class="fotouser">
                         <div class="datosuser">
-                            <label class="labelnombre">{{ $usuario['nombre'] }}</label>
-                            <label class="labelcorreo">{{ $usuario['email'] }}</label>
-                            <label class="labelWeb">{{ $usuario['web'] }}</label>
+                            <label class="labelnombre col-md-12 col-xs-12">{{ $usuario['nombre'] }}</label>
+                            <label class="labelcorreo col-md-12 col-xs-12">{{ $usuario['email'] }}</label>
+                            <label class="labelWeb col-md-12 col-xs-12">{{ $usuario['web'] }}</label>
                         </div>
                         <img src="img/editar.png" class="enlaceeditar">
                     </div>
                 </div>
                 <!-- contenido de la pagina -->
-                <div class="panel-body ofertas">
-                    <div class="oferta">
+                <div class="panel-body ofertas col-md-12 ">
+                    <div class="oferta col-md-12">
                         <h2 class="tituloofertas">Ofertas disponibles</h2>
                         @foreach( $ofertas as $oferta )
                         <hr/>
