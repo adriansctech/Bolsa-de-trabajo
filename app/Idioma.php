765c010ff08bc52012cc173bb2e3450c6bdbf3c2
Sergio <sergio@prueba>
2017-02-16 16:52:12 +0100

    programacion vistas perfil alumno mostrar/editar

---
+++ app/Idioma.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Bolsa;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Idioma extends Model
+{
+    protected $fillable = ['id', 'idioma'];
+}
--- resources/views/perfiles/editar/alumno.blade.php
+++ resources/views/perfiles/editar/alumno.blade.php
@@ -5,7 +5,7 @@
 		<main>
 			<div class="Principal">
 				<h1>Perfil en edición</h1>
-				<form action="" method="">
+				<form action="" method="" >
 					<div class="imagen">
 					<!--Foto de prefil-->
 						<img src="../img/user.jpg">
@@ -50,40 +50,69 @@
 
 					<div class="ciclos">
 						<label>Ciclos cursados en Batoi</label>
-						<!--Checbox con todos los ciclos de batoy-->
-						<div id="ciclosBatoy">
-							<input type="checkbox" name="ciclo" value="ciclo1">ciclo1 <br>
-							<input type="checkbox" name="ciclo" value="ciclo2" checked> ciclo2<br>					
+						<!-- lista de los ciclos guardados -->
+						<strong>
+							<ul id="listaCiclos"></ul>
+						</strong>
+						<!--select con todos los ciclos de batoy-->
+						<div id="ciclosBatoy">	
+							<select id="selectCiclos" name="cicloElegido" onchange="valorCambiadoSelect(this.value)">
+								<!-- valor por defecto -->
+								<option value="defecto" selected>- Elige un ciclo -</option>
+								@foreach ($ciclos as $ciclo)
+									<option value="{{ $ciclo['ciclos'] }}">{{ $ciclo['ciclos'] }}</option>
+								@endforeach
+							</select>
+							<br>
 						</div>
 					</div>
-
-					<div class="ciclosCursados">
-						<h4>Datos opcionales</h4>
+					
+					<div class="ciclosCursados" id="infoCiclos" style="display: none;">
+					<!--	<h4>Datos opcionales</h4>
 						<div class="masInfo">
 						<span class="adicional"><small>Añade un poco más de información sobre ti para que las empresas puedan <br/> encontrarte más facilmente y contactar contigo.</small></span>
-						</div>
+						</div>-->
 						<!--Div en el que cargamos los ciclos que ha marcado el alumno como que los ha cursado-->
-						<h3>Ciclo_x</h3>
+						<br />
+						<h4 id="tituloCiclo"></h4>
 						<label>Fecha finalizacion:</label>
-						<input type="date" name="finCiclo_x">
+						<input type="date" name="finCiclo" id="fechaCiclo">
 						<br/>
 						<label>Nota ciclo:</label>
-						<input type="number" name="notaCiclo_x">
+						<input type="number" name="notaCiclo" id="notaCiclo">
 						<br/>
 						<label>Empresa de las FCT:</label>
-						<input type="text" name="FCTCiclo_x">
+						<input type="text" name="FCTCiclo" id="empresaCiclo">
 						<br/>
-						<hr>
+						<input type="button" id="anyadirCiclo" name="anyadirCiclo" value="Añadir" onclick="guardarCiclo()">
+						<!-- <hr> -->
 					</div>
-
+					<br>
 					<div class="datosSecundarios">
 						<!--Div de los idiomas, opcion de trabajar fuera y CV-->
-						<span>Selecciona los idiomas con sus niveles</span><br/>
-						<label>Idiomas:</label>
-						<select>
-						  <option value="volvo" selected>Selecciona </option>
+						
+						<label>Selecciona los idiomas con sus niveles</label>
+						<!-- lista de los idiomas guardados -->
+						<strong>
+							<ul id="listaIdiomas"></ul>
+						</strong>
+						
+						@foreach ($idiomas as $idioma)
+							<input type="radio" name="idioma[]" id="{{ $idioma }}" value="{{ $idioma['idioma'] }}" onclick="checkIdioma()"> {{ $idioma['idioma'] }}
+							<br>
+						@endforeach
+						
+						<select id="nivelIdioma" style="display: none;">
+							<option value="defecto">- Elige una opción -</option>
+						  	<option value="A1">A1</option>
+						  	<option value="A2">A2</option>
+						  	<option value="B1">B1</option>
+						  	<option value="B2">B2</option>
+						  	<option value="C1">C1</option>
+						  	<option value="C2">C2</option>
 						</select>
-						<br/>
+						<input type="button" id="anyadirIdioma" name="anyadirIdioma" value="Añadir" onclick="guardarIdioma()" style="display: none;">
+						<br>
 						<label>Con opción a trabajar fuera:</label>
 						@if($usuario['trabajoFuera'] == 0)
 							<input type="checkbox" name="trabajoFuera" value="1">Si
@@ -105,3 +134,83 @@
 		</main>		
 	</div>	
 	@endsection	
+	<!-- obtener el vaor del select -->
+	<script type="text/javascript">
+		//obtener el valor del select al cambiar de estado
+		function valorCambiadoSelect(val){  
+    		if(val == "defecto"){
+    			//ocultar el div de los datos si el valor es el de defecto
+				document.getElementById("infoCiclos").style.display = 'none';
+    		}else{
+    			//enviar el valor a la etiqueta 
+				document.getElementById("tituloCiclo").innerHTML = val;
+				//mostrar el div de los datos al cambiar el estado del select
+				document.getElementById("infoCiclos").style.display = '';
+			}
+		}
+		//guardar en la lista el ciclo
+		function guardarCiclo(){
+			var ciclo = document.getElementById("tituloCiclo").innerHTML;
+			var fecha = document.getElementById("fechaCiclo").value;
+			var nota = document.getElementById("notaCiclo").value;
+			var empresa = document.getElementById("empresaCiclo").value;
+			//crear li para la lista
+			var ul = document.getElementById("listaCiclos");
+			var li = document.createElement("li");
+			//contenido del li
+			li.appendChild(document.createTextNode(ciclo+" - Fecha finalización: "+fecha+" - Nota: "+nota+" - Empresa: "+empresa));
+			ul.appendChild(li);
+			//vaciar los inputs y camibar el valor del select
+			document.getElementById("selectCiclos").selectedIndex = 0;
+			document.getElementById("fechaCiclo").value = "";
+			document.getElementById("notaCiclo").value = "";
+			document.getElementById("empresaCiclo").value = "";
+			//mostrar el div de los datos al cambiar el estado del select
+			document.getElementById("infoCiclos").style.display = 'none';
+		}
+		//mostrar el desplegable del idioma
+		function checkIdioma(){
+			//mostrar el select de los datos y el boton
+			document.getElementById("nivelIdioma").style.display = '';
+			document.getElementById("anyadirIdioma").style.display = '';
+		}
+		//guardar el idioma
+		function guardarIdioma(){
+			//obtener los radios
+			var radios = document.getElementsByName('idioma[]');
+			//buscar el radio seleccionado
+			for (var i = 0, length = radios.length; i < length; i++) {
+			    if (radios[i].checked) {
+			    	//encontrado el radio y obtener el valor
+			        var radioPulsado = radios[i].value;
+			        break;
+			    }
+			}
+			//obtener valor del select
+			var selectRadio = document.getElementById("nivelIdioma");
+			var idiomaSeleccionado = selectRadio.options[selectRadio.selectedIndex].value;
+
+			if(idiomaSeleccionado == "defecto"){
+				//mensaje informativo
+				alert("Seleccione un nivel del idioma.")
+			}else{	
+				//crear li para la lista
+				var ul = document.getElementById("listaIdiomas");
+				var li = document.createElement("li");
+				//contenido del li
+				li.appendChild(document.createTextNode(radioPulsado+" - "+idiomaSeleccionado));
+				ul.appendChild(li);
+				//vaciar los inputs y camibar el valor del select
+				document.getElementById("nivelIdioma").selectedIndex = 0;
+				//quitar valor del radio
+				for (var i = 0, length = radios.length; i < length; i++) {
+			    	radios[i].checked = false;
+			    }
+			    //ocultar el select y el boton
+			    document.getElementById("nivelIdioma").style.display = 'none';
+				document.getElementById("anyadirIdioma").style.display = 'none';
+			}
+
+			}
+		
+	</script>
