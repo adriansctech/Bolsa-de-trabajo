7caa21175b7f494be61d17e2dcfe348939e23fad
adriansctehc <adriansanchezcarchano@gmail.com>
2017-02-16 16:25:14 +0100

    modifcando html de responsablePrincipal

--- resources/views/principales/responsable.blade.php
+++ resources/views/principales/responsable.blade.php
@@ -3,25 +3,29 @@
 @section('content')
 <div class="container contenido">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
+        <div class="col-xs-12 col-md-8 col-md-offset-2">
+            <div class="col-xs-12 panel panel-default">
                 <!-- cabecera de la pagina -->
-                <div class="panel-heading" >
-                    <div class="infouser">
-                        <img src="img/user.jpg" class="fotouser">
-                        <div class="datosuser">
-                            <label class="labelnombre">{{ $usuario['nombre'] }}</label>
-                            <label class="labelcorreo">{{ $usuario['email'] }}</label>
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-12 col-md-offset-1 panel-heading" >
+                    <div class="col-xs-12 col-md-4 col-md-offset-1">
+                        <img src="img/user.jpg" class="imagen">
+                        <div class="col-xs-12 col-md-6 datosPrincipales">
+                            <label id="labelNombre">Nombre Apellido1 Apellido2</label><br>
+                            <label id="labelCorreo">usuario@gmail.com</label>
                         </div>
                         <img src="img/editar.png" class="enlaceeditar">
+                    </div> 
+                </div>
+                 <div>                    
+                    <div>
+                        <input type="submit" value="Confirmar alumnos">
+                        <input type="submit" value="Confirmar ofertas">
                     </div>
-                </div>
-                <!-- contenido de la pagina -->
-                <div class="panel-body ofertas">
-                    <div class="oferta">
-                        <h2 class="tituloofertas">Ofertas disponibles</h2>
-                    </div>  
-                </div>
+                    <div>
+                        <input type="submit" value="Dar de alta empresa">
+                        <input type="submit" value="Listar empresas">  
+                    </div>
+                </div>                  
             </div>
         </div>
     </div>
