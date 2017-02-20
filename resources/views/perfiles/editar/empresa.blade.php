10123d810491f3e270c04eebf5a5c027f858d03e
Aonpei <aonpei@gmail.com>
2017-02-20 19:11:48 +0100

    Rutas oferta empresa y editar empresa, cargar datos en editar perfil de empresa

--- resources/views/perfiles/editar/empresa.blade.php
+++ resources/views/perfiles/editar/empresa.blade.php
@@ -8,34 +8,34 @@
                 <!-- cabecera de la pagina -->
                 <div class="panel-heading col-md-10 col-md-offset-1 " >
 	                <h1>Perfil en edición</h1>
-					<form action="" method="">
+					<form action="{{ url('/empresa/perfil') }}" method="POST">
 					<div class="col-xs-10 col-sm-offset-1 col-sm-4 col-sm-offset-1 col-md-4 col-md-offset-1">
 						<!--Foto de prefil-->
-						<img src="../img/user.jpg " class="imagen ">
+						<img src="{{$usuario['logo']}}" class="imagen ">
 					</div>
 
 					<div class="datosPrincipales col-xs-12 col-sm-6 col-md-6 "> 
 						<!-- Datos de la empresa parte 1-->
 						<label for="name">Nombre:</label>
-						<input type="text" name="nombre" id="name">
+						<input type="text" name="nombre" id="name" value="{{ $usuario['nombre'] }}">
 						<br/>
 						<label for="CIF">CIF</label>
-						<input type="text" name="cif" id="CIF">
+						<input type="text" name="cif" id="CIF" value="{{ $usuario['cif'] }}">
 						<br/>
 						<label for="address">Domicilio</label>
-						<input type="text" name="domicilio" id="address">
+						<input type="text" name="domicilio" id="address" value="{{ $usuario['domicilio'] }}">
 						<br/>
 						<label for="phone">Telefono</label>
-						<input type="tel" name="telefono" size="9" id="phone">
+						<input type="tel" name="telefono" size="9" id="phone" value=" {{ $usuario['tlf'] }}">
 						<br/>
 						<label for="email">Email</label>
-						<input type="email" name="email" id="email">
+						<input disabled type="email" name="email" id="email" value="{{ $usuario['email'] }}">
 						<br/>
 						<label for="web">Web</label>
-						<input type="url" name="web" id="web">
+						<input type="url" name="web" id="web" value="{{ $usuario['web'] }}">
 						<br/>
 						<label for="sector">Sector</label>
-						<input type="text" name="sector" id="sector">
+						<input type="text" name="sector" id="sector" value="{{ $usuario['sector'] }}">
 						<br/>
 					</div>
 				</div>
@@ -61,16 +61,16 @@
 						<!--Div donde vamos a poner la persona de contacto de la empresa-->
 						<h2>Persona de contacto</h2>
 						<label for=contactname>Nombre :</label>
-						<input type="text" name="nombrePC" id="contactname">
+						<input type="text" name="nombreContacto" id="contactname" value="{{ $usuario['nombreContacto'] }}">
 						<br/>
 						<label for="contactposition">Cargo :</label>
-						<input type="text" name="cargoPC" id="contactposition">
+						<input type="text" name="cargoContacto" id="contactposition" value="{{ $usuario['cargoContacto'] }}">
 						<br/>
 						<label for="contactphone">Telefono</label>
-						<input type="tel" name="telefonoPC" id="contactphone">
+						<input type="tel" name="tlfContacto" id="contactphone" value="{{ $usuario['tlfContacto'] }}">
 						<br/>
 						<label for="contactemail">Email</label>
-						<input type="email" name="emailPC" id="contactemail">
+						<input type="email" name="emailContacto" id="contactemail" value="{{ $usuario['emailContacto'] }}">
 						<br/>
 					</div>
 
