7ac3522681ef9703ccdbf8ccf08216a1da8930df
vagrant <vagrant@homestead>
2017-02-21 15:54:09 +0000

    hacer que en editar responsable aparezcan datos

--- resources/views/perfiles/editar/responsable.blade.php
+++ resources/views/perfiles/editar/responsable.blade.php
@@ -8,20 +8,20 @@
                 <div class="col-xs-12  col-md-12  panel-heading" >
                 	<h1>Datos del responsable</h1>
                 	<div class="col-xs-12  col-md-4 col-md-offset-1">
-						<img src="/img/user.jpg " class="imagen ">
+						<img src=src="{{$usuario['foto']}}" class="imagen ">
                 	</div>                	
                 	<div class="datosPrincipales cambioContraseña"> 
 	                	<label for="nameR">Nombre:</label>
-						<input type="text" name="nombre" id="nameR">
+						<input type="text" name="nombre"  id="nameR" value="{{ $usuario['nombre'] }}">
 						<br/>
 						<label for="phoneR">Telefono: </label>
-						<input type="tel" name="telefono" size="9" id="phoneR">
+						<input type="tel" name="telefono" size="9" id="phoneR" value="{{ $usuario['tlf'] }}" >
 						<br/>
 						<label for="emailR">Email: </label>
-						<input type="email" name="email" id="emailR">
+						<input type="email" name="email" id="emailR" value="{{ $usuario['email'] }}" >
 						<br/>
 						<label for="apassR">Contraseña actual:</label>
-						<input type="password" name="contraseñaActual" id="apassR">
+						<input type="password" name="contraseñaActual" id="apassR" >
 						<br/>
 						<label for="npassR">Nueva Contraseña:</label>
 						<input type="password" name="nuevaContraseña" id="npassR">
