8cbcbd5e1208647ef53c29ce383782cf624d9fe1
vagrant <vagrant@homestead>
2017-02-27 17:26:58 +0000

    media.css pasado a less

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
 
     <!-- Styles -->    
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel='stylesheet/less' type='text/css' href="/css/media.css" />
+    <link rel='stylesheet/less' type='text/css' href="/css/media.less" />
     <link rel='stylesheet/less' type='text/css' href="/css/custom.less" /> 
     <link rel='stylesheet/less' type='text/css' href="/css/paginas_principales_usuarios.less" />    
     <link rel='stylesheet/less' type='text/css' href="/css/style.less" />
